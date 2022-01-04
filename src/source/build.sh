d14622ff48dcfe7c9269126e8397191a36549a1c
John S. Urban <urbanjost@comcast.net>
2022-01-03 21:27:17 -0500

    refresh

--- src/source/build.sh
+++ src/source/build.sh
@@ -7,4 +7,6 @@ export DEMO_OUTDIR=../../example/
 export DEMO_SUBDIR=FALSE
 GPF_build_module M_random
 )
+cp ../../docs/man3.html ../../docs/index.html
+cp ../../docs/BOOK_M_random.html ../../docs/index.html
 exit
