1293d1802a939c52ee4b33248a141f3d433099a4
John S. Urban <urbanjost@comcast.net>
2022-01-08 17:43:05 -0500

    example apps

--- app/rand.f90
+++ app/rand.f90
@@ -98,7 +98,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)AUTHOR:         John S. Urban>',&
 '@(#)LICENSE:        Public Domain. This is free software: you are free to change and redistribute it.>',&
 '@(#)                There is NO WARRANTY, to the extent permitted by law.>',&
-'@(#)COMPILED:       2022-01-08 17:32:18 UTC-300>',&
+'@(#)COMPILED:       2022-01-08 17:38:41 UTC-300>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i))-1)),i=1,size(help_text))
    stop ! if --version was specified, stop
