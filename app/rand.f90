85cb6dace494462fe453852a0054911968a14756
John S. Urban <urbanjost@comcast.net>
2023-04-25 03:42:11 -0400

    rand

--- app/rand.f90
+++ app/rand.f90
@@ -2,11 +2,11 @@
 !()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()!
 !===================================================================================================================================
 program rand
-use M_io,      only : fileread
-use M_strings, only : notabs
-use M_random,  only : scramble, init_random_seed_by_system_clock
-use M_framework__verify,   only : stderr
-use M_kracken, only : kracken, lget, sgets, iget, igets             ! add command-line parser module
+use M_io,        only : fileread
+use M_strings,   only : notabs
+use M_random,    only : scramble, init_random_seed_by_system_clock
+use M_framework, only : stderr
+use M_kracken,   only : kracken, lget, sgets, iget, igets             ! add command-line parser module
 implicit none
 character(len=4096),allocatable  :: FILENAMES(:)
 character(len=:),allocatable     :: pageout(:) ! array to hold file in memory
